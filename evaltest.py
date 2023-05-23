2ff01282b9a53232631ce7660fbf7319a24edf10
atulya raaj <atulraj1010@tutanota.com>
2023-05-24 00:17:03 +0530

    location

---
+++ evaltest.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from streamlit_js_eval import get_geolocation
+import json
+
+             
+if st.checkbox("Check my location"):
+    loc = get_geolocation()
+    st.write(f"Your coordinates are {loc}")
